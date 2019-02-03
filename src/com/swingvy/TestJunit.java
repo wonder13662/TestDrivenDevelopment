dd39e25344fb8799a3cd2e71f96f6f6ecf884e0d
Wonder Jung <wonder13662@gmail.com>
2019-02-03 20:52:15 +0900

    51p step2 (Done)$5 x 2 = $10

--- src/com/swingvy/TestJunit.java
+++ src/com/swingvy/TestJunit.java
@@ -9,7 +9,7 @@ public class TestJunit {
    
    // TODO
    // $5 + 10CHF = $10(환율이 2:1일 경우)
-   // (NOW) $5 x 2 = $10
+   // (DONE) $5 x 2 = $10
    // amount를 private으로 만들기
    // Dollar 부작용(side effect)?
    // Money 반올림?
