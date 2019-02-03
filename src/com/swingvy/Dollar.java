0dd322a0e3ebde91c1def80313d33a3c9ceb8e62
Wonder Jung <wonder13662@gmail.com>
2019-02-03 22:34:40 +0900

    77p Money 클래스에 amount 변수 옮김. Dollar 클래스가 Money 클래스를 상속받음

--- src/com/swingvy/Dollar.java
+++ src/com/swingvy/Dollar.java
@@ -4,10 +4,7 @@ package com.swingvy;
  * 값 객체 패턴(value object pattern): 객체의 인스턴스 변수가 생성자를 통해서 일단 설정된 후에는 결코 변하지 않는다.(59p)
  */
 
-public class Dollar {
-	
-	private int amount;
-
+public class Dollar extends Money {
 	public Dollar(int amount) {
 		this.amount = amount;
 	}
--- src/com/swingvy/Money.java
+++ src/com/swingvy/Money.java
@@ -1,5 +1,5 @@
 package com.swingvy;
 
 public class Money {
-
+	protected int amount;
 }
