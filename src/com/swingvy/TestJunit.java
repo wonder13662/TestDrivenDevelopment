554c3c0181f82d4055a6859b2bf155bdd3e3358e
Wonder Jung <wonder13662@gmail.com>
2019-02-04 00:12:33 +0900

    90p Money 객체를 사용하는 곳을 팩토리 메서드로 변경

--- src/com/swingvy/TestJunit.java
+++ src/com/swingvy/TestJunit.java
@@ -28,18 +28,18 @@ public class TestJunit {
    @Test
    public void testMultipleication() {
 	   Money five = Money.dollar(5);
-	   assertEquals(new Dollar(10), five.times(2));
-	   assertEquals(new Dollar(15), five.times(3));
+	   assertEquals(Money.dollar(10), five.times(2));
+	   assertEquals(Money.dollar(15), five.times(3));
    }
    
    @Test
    public void testEquality() {
 		// 삼각측량
-	   assertTrue(new Dollar(5).equals(new Dollar(5)));
-	   assertFalse(new Dollar(5).equals(new Dollar(6)));
+	   assertTrue(Money.dollar(5).equals(Money.dollar(5)));
+	   assertFalse(Money.dollar(5).equals(Money.dollar(6)));
 	   assertTrue(new Franc(5).equals(new Franc(5)));
 	   assertFalse(new Franc(5).equals(new Franc(6)));
-	   assertFalse(new Franc(5).equals(new Dollar(5)));
+	   assertFalse(new Franc(5).equals(Money.dollar(5)));
    }
    
    @Test
