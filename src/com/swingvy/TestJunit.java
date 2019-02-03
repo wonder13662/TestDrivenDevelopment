55921bed711860e9613583a7915445dd350c1fd4
Wonder Jung <wonder13662@gmail.com>
2019-02-03 22:43:10 +0900

    79p testEquality 메서드 안에서 Franc의 검사조건 추가

--- src/com/swingvy/TestJunit.java
+++ src/com/swingvy/TestJunit.java
@@ -35,6 +35,8 @@ public class TestJunit {
 		// 삼각측량
 	   assertTrue(new Dollar(5).equals(new Dollar(5)));
 	   assertFalse(new Dollar(5).equals(new Dollar(6)));
+	   assertTrue(new Franc(5).equals(new Franc(5)));
+	   assertFalse(new Franc(5).equals(new Franc(6)));  
    }
    
    @Test
