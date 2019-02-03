ec704f37ee5b97a30063cb2efa3a38dc3ac4ffb9
Wonder Jung <wonder13662@gmail.com>
2019-02-03 22:40:40 +0900

    78p Dollar 클래스의 equal 메서드를 상위 클래스인 Money 클래스로 이동

--- src/com/swingvy/Dollar.java
+++ src/com/swingvy/Dollar.java
@@ -12,9 +12,4 @@ public class Dollar extends Money {
 	Dollar times(int multiplier) {
 		return new Dollar(amount * multiplier);
 	}
-	
-	public boolean equals(Object object) {
-		Money money = (Money) object;
-		return amount == money.amount;
-	}
 }
--- src/com/swingvy/Money.java
+++ src/com/swingvy/Money.java
@@ -2,4 +2,9 @@ package com.swingvy;
 
 public class Money {
 	protected int amount;
+	
+	public boolean equals(Object object) {
+		Money money = (Money) object;
+		return amount == money.amount;
+	}	
 }
