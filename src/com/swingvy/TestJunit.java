3d29a6dc8f17dcf5420f4b754bf69c9007513184
Wonder Jung <wonder13662@gmail.com>
2019-02-03 20:58:19 +0900

    54p step1 (WORKING)Dollar 부작용

--- src/com/swingvy/TestJunit.java
+++ src/com/swingvy/TestJunit.java
@@ -11,7 +11,7 @@ public class TestJunit {
    // $5 + 10CHF = $10(환율이 2:1일 경우)
    // (DONE) $5 x 2 = $10
    // amount를 private으로 만들기
-   // Dollar 부작용(side effect)?
+   // (WORKING) Dollar 부작용(side effect)?
    // Money 반올림?
    
    @Test
