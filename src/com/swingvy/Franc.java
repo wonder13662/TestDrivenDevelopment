569de5cfb381c72e4257b937ec60e6c7e0713209
Wonder Jung <wonder13662@gmail.com>
2019-02-03 22:45:05 +0900

    79p Franc 클래스의 amount 필드를 삭제. Franc 클래스가  Money 클래스를 상속하도록 변경

--- src/com/swingvy/Franc.java
+++ src/com/swingvy/Franc.java
@@ -4,10 +4,7 @@ package com.swingvy;
  * 값 객체 패턴(value object pattern): 객체의 인스턴스 변수가 생성자를 통해서 일단 설정된 후에는 결코 변하지 않는다.(59p)
  */
 
-public class Franc {
-	
-	private int amount;
-
+public class Franc extends Money {	
 	public Franc(int amount) {
 		this.amount = amount;
 	}
