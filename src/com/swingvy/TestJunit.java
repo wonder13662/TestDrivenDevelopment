3cb427e1d9120a862100ef1d75cdeb69181ab6fa
Wonder Jung <wonder13662@gmail.com>
2019-02-03 22:18:20 +0900

    72p TODO List 추가:Dollar/France 중복, 공용 equals, 공용 times

--- src/com/swingvy/TestJunit.java
+++ src/com/swingvy/TestJunit.java
@@ -19,6 +19,9 @@ public class TestJunit {
 	// Equal null
 	// Equal object
 	// (DONE) 5CHF x 2 = 10CHF
+	// Dollar/Franc 중복
+	// 공용 equals
+	// 공용 times
    
    @Test
    public void testMultipleication() {
