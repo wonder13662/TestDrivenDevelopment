78db8f598228483d5d6229126f5a799b8569f066
Wonder Jung <wonder13662@gmail.com>
2019-02-03 22:06:23 +0900

    67p TODO List 업데이트:(DONE)amount를 private으로 만들기

--- src/com/swingvy/TestJunit.java
+++ src/com/swingvy/TestJunit.java
@@ -11,7 +11,7 @@ public class TestJunit {
    // TODO
    // $5 + 10CHF = $10(환율이 2:1일 경우)
    // (DONE) $5 x 2 = $10
-   // amount를 private으로 만들기
+   // (DONE) amount를 private으로 만들기
    // (DONE) Dollar 부작용(side effect)?
    // Money 반올림?
 	// (DONE) equals()
