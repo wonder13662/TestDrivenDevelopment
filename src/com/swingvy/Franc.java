4ab7fdb1182b93932057b7c649cc1e5762d8aef6
Wonder Jung <wonder13662@gmail.com>
2019-02-03 22:48:59 +0900

    80p Franc 클래스의 equal 메서드 수정. Casting을 Money 클래스로 함.

--- src/com/swingvy/Franc.java
+++ src/com/swingvy/Franc.java
@@ -14,7 +14,7 @@ public class Franc extends Money {
 	}
 	
 	public boolean equals(Object object) {
-		Franc dollar = (Franc) object;
-		return amount == dollar.amount;
+		Money money = (Money) object;
+		return amount == money.amount;
 	}
 }
