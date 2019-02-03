850e88983e162654a50621d6170295d2a6cb1686
Wonder Jung <wonder13662@gmail.com>
2019-02-03 20:44:26 +0900

    49p step2

--- src/com/swingvy/Dollar.java
+++ src/com/swingvy/Dollar.java
@@ -2,13 +2,13 @@ package com.swingvy;
 
 public class Dollar {
 	
-	public int amount = 5*2;
+	public int amount;
 
 	public Dollar(int amount) {
 	}
 	
-	public void times(int multiplier) {
-		
+	void times(int multiplier) {
+		amount = 5 * 2;
 	}
 
 }
