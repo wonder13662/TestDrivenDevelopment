4ad0d2c2e35fcf48cb8ac8cf91a4242cdfd03199
Wonder Jung <wonder13662@gmail.com>
2019-02-03 22:58:47 +0900

    84p Dollar와 Franc을 비교하는 테스트 코드 추가

--- src/com/swingvy/TestJunit.java
+++ src/com/swingvy/TestJunit.java
@@ -37,7 +37,8 @@ public class TestJunit {
 	   assertTrue(new Dollar(5).equals(new Dollar(5)));
 	   assertFalse(new Dollar(5).equals(new Dollar(6)));
 	   assertTrue(new Franc(5).equals(new Franc(5)));
-	   assertFalse(new Franc(5).equals(new Franc(6)));  
+	   assertFalse(new Franc(5).equals(new Franc(6)));
+	   assertFalse(new Franc(5).equals(new Dollar(5)));
    }
    
    @Test
