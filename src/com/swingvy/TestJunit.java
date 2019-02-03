130912db758fdedae43c97fe4ca61f0b6d1faf9b
Wonder Jung <wonder13662@gmail.com>
2019-02-03 23:10:32 +0900

    85p TODO List 업데이트:(DONE)Franc과 Dollar 비교하기,통화?

--- src/com/swingvy/TestJunit.java
+++ src/com/swingvy/TestJunit.java
@@ -22,7 +22,7 @@ public class TestJunit {
 	// Dollar/Franc 중복
 	// (DONE)공용 equals
 	// 공용 times
-	// (WORKING)France과 Dollar 비교하기
+	// (DONE)France과 Dollar 비교하기
    
    @Test
    public void testMultipleication() {
