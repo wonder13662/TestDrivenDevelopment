9dbfd32dbc72ed23ff8b652787422c070a21dc74
Wonder Jung <wonder13662@gmail.com>
2019-02-06 23:16:46 +0900

    126p Bank 클래스의 reduce 메서드의 조건문 추가.reduce 테스트 추가.

--- src/com/swingvy/Bank.java
+++ src/com/swingvy/Bank.java
@@ -2,6 +2,7 @@ package com.swingvy;
 
 public class Bank {
 	Money reduce(Expression source, String to) {
+		if(source instanceof Money) return (Money) source;
 		Sum sum = (Sum) source;
 		return sum.reduce(to);
 	}
