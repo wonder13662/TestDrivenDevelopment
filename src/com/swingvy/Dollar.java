af2c9da7f15bbfc410f0f930942e6caf5df4f3cf
Wonder Jung <wonder13662@gmail.com>
2019-02-06 13:40:35 +0900

    103p times 메서드의 Franc, Dollar의 생성자의 파라미터를 currency 변수로 변경

--- src/com/swingvy/Dollar.java
+++ src/com/swingvy/Dollar.java
@@ -10,6 +10,6 @@ public class Dollar extends Money {
 	}
 	
 	Money times(int multiplier) {
-		return new Dollar(amount * multiplier,  "USD");
+		return new Dollar(amount * multiplier,  currency);
 	}
 }
