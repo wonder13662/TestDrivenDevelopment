da0c3b58f742f5452e9960a517979c5879898dc1
Wonder Jung <wonder13662@gmail.com>
2019-02-06 23:26:42 +0900

    127p TODO List 업데이트:(DONE) Bank.reduce(Money)

--- src/com/swingvy/TestJunit.java
+++ src/com/swingvy/TestJunit.java
@@ -9,9 +9,10 @@ import org.junit.Test;
 public class TestJunit {
 	
    // TODO
-   // (WORKING) $5 + 10CHF = $10(환율이 2:1일 경우)
-   // (WORKING) $5 + $5 = $10(환율이 2:1일 경우)
+   // $5 + 10CHF = $10(환율이 2:1일 경우)
+   // $5 + $5 = $10(환율이 2:1일 경우)
 	// $5 + $5에서 Money 반환하기
+	// (DONE) Bank.reduce(Money)
    // (DONE) $5 x 2 = $10
    // (DONE) amount를 private으로 만들기
    // (DONE) Dollar 부작용(side effect)?
