77e79db62ffd802c687d2917deba453a430a54c0
Wonder Jung <wonder13662@gmail.com>
2019-02-13 04:39:31 +0900

    134p 같은 환율일 경우에 rate를 1을 돌려주도록 고침.Bank 클래스에서 Pair 객체를 사용하도록 고침

--- src/com/swingvy/Bank.java
+++ src/com/swingvy/Bank.java
@@ -1,11 +1,21 @@
 package com.swingvy;
 
+import java.util.Hashtable;
+
 public class Bank {
+	private Hashtable<Pair, Integer> rates = new Hashtable<Pair, Integer>();
+	
 	Money reduce(Expression source, String to) {
 		return source.reduce(this, to);
 	}
 	
 	int rate(String from, String to) {
-		return (from.equals("CHF") && to.equals("USD"))?2:1;
+		if(from.equals(to)) return 1;
+		Integer rate = (Integer) rates.get(new Pair(from, to));
+		return rate.intValue();
+	}
+	
+	void addRate(String from, String to, int rate) {
+		rates.put(new Pair(from, to),  new Integer(rate));
 	}
 }
--- src/com/swingvy/TestJunit.java
+++ src/com/swingvy/TestJunit.java
@@ -86,8 +86,13 @@ public class TestJunit {
    @Test
    public void testReduceMoneyDifferentCurrency() {
 	   Bank bank = new Bank();
-	   // bank.addRate("CHF", "USD", 2);
+	   bank.addRate("CHF", "USD", 2);
 	   Money result = bank.reduce(Money.franc(2), "USD");
 	   assertEquals(Money.dollar(1), result);
    }   
+   
+   @Test
+   public void testIdentityRate() {
+	   assertEquals(1, new Bank().rate("USD", "USD"));
+   }
 }
