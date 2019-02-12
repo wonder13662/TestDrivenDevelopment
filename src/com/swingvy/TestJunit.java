b2b54a905c668938fb3294d0cc2f55ed00c0d4ab
Wonder Jung <wonder13662@gmail.com>
2019-02-13 04:48:30 +0900

    138p 다른 환율들을 더하는 테스트 케이스. 컴파일 실패.

--- src/com/swingvy/TestJunit.java
+++ src/com/swingvy/TestJunit.java
@@ -95,4 +95,14 @@ public class TestJunit {
    public void testIdentityRate() {
 	   assertEquals(1, new Bank().rate("USD", "USD"));
    }
+   
+   @Test
+   public void testMixedAddition() {
+	   Expression fiveBucks = Money.dollar(5);
+	   Expression tenFrancs = Money.franc(10);
+	   Bank bank = new Bank();
+	   bank.addRate("CHF", "USD", 2);
+	   Money result = bank.reduce(fiveBucks.plus(tenFrancs), "USD");
+	   assertEquals(Money.dollar(10), result);
+   }
 }
