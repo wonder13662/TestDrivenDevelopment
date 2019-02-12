c279e31ab7cf004887efacac4c544842de93a0ee
Wonder Jung <wonder13662@gmail.com>
2019-02-13 05:03:00 +0900

    143p TODO list 업데이트

--- src/com/swingvy/TestJunit.java
+++ src/com/swingvy/TestJunit.java
@@ -9,10 +9,11 @@ import org.junit.Test;
 public class TestJunit {
 	
    // TODO
-   // $5 + 10CHF = $10(환율이 2:1일 경우)
+   // (DONE) $5 + 10CHF = $10(환율이 2:1일 경우)
    // (DONE) $5 + $5 = $10
 	// $5 + $5에서 Money 반환하기
 	// Sum.plus의 구현
+	// Expression.times
 	// (DONE) Bank.reduce(Money)
 	// (DONE) Money에 대한 통화 변환을 수행하는 Reduce
 	// (DONE) Reduce(Bank, String)
