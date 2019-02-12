193568a7bc95cf4db917bf18bc3c3fb0f031eccb
Wonder Jung <wonder13662@gmail.com>
2019-02-13 04:22:09 +0900

    132p 환율 계산을 Bank 객체가 하도록 변경

--- src/com/swingvy/Bank.java
+++ src/com/swingvy/Bank.java
@@ -2,6 +2,10 @@ package com.swingvy;
 
 public class Bank {
 	Money reduce(Expression source, String to) {
-		return source.reduce(to);
+		return source.reduce(this, to);
+	}
+	
+	int rate(String from, String to) {
+		return (from.equals("CHF") && to.equals("USD"))?2:1;
 	}
 }
--- src/com/swingvy/Expression.java
+++ src/com/swingvy/Expression.java
@@ -1,5 +1,5 @@
 package com.swingvy;
 
 public interface Expression {
-	Money reduce(String to);
+	Money reduce(Bank bank, String to);
 }
